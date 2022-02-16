f9a46b3f2279d97462caaa674b8120b69222eae3
HongPhuc269 <nguyennhuhongphuc269@gmail.com>
2022-02-16 10:27:14 +0700

    Second Attempt

--- Logmein/app/src/main/java/com/codewithphuc/logmein/MainActivity.java
+++ Logmein/app/src/main/java/com/codewithphuc/logmein/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
     private EditText txtUser;
     private EditText txtPass;
     private Button button;
-
+    private ImageView img,imgAndr;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,6 +24,9 @@ public class MainActivity extends AppCompatActivity {
         txtPass = findViewById(R.id.txtPass);
         button = findViewById(R.id.txtLog);
 
+        img = findViewById(R.id.img);
+        imgAndr = findViewById(R.id.imgAnd);
+
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -30,6 +34,7 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(MainActivity.this, "Welcome back!", Toast.LENGTH_SHORT).show();
                 else{
                     Toast.makeText(MainActivity.this, "Incorrect username or password!", Toast.LENGTH_SHORT).show();
+                    imgAndr.setVisibility(View.VISIBLE);
                 }
             }
         });
--- Tut2/app/src/main/java/com/codewithphuc/tut2/MainActivity.java
+++ Tut2/app/src/main/java/com/codewithphuc/tut2/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -13,19 +14,23 @@ public class MainActivity extends AppCompatActivity {
     private TextView txtQues;
     private EditText txtName;
     private Button button;
+    private ImageView imgTheme,imgAnd;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        txtQues = findViewById(R.id.txtQuestion);
+        txtQues = findViewById(R.id.txtQues);
         txtName = findViewById(R.id.txtName);
-        button =findViewById(R.id.txtBtn);
+        button =findViewById(R.id.btnClick);
+        imgTheme = findViewById(R.id.imgTheme);
+        imgAnd = findViewById(R.id.imgAnd);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Toast.makeText(MainActivity.this,"Hello " + txtName.getText().toString(),Toast.LENGTH_SHORT).show();
+                imgAnd.setVisibility(View.VISIBLE);
             }
         });
     }
